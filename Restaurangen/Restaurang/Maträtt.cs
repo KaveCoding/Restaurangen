9efe57d6e6543fe2ce0d441ebdc2b9d42dfe4c1e
KaveCoding <teachereli@outlook.com>
2022-10-31 13:49:10 +0100

    Tog bort allergener

--- Restaurangen/Restaurang/Maträtt.cs
+++ Restaurangen/Restaurang/Maträtt.cs
@@ -10,15 +10,13 @@ namespace Restaurangen.Restaurang
     {
         public string Namn { get; set; }
         public int Pris { get; set; }
-        public List<string> Allergener { get; set; }
         public int Kvalité { get; set; }
         public int Destination_bord { get; set; }
 
-        Maträtt(string namn, int pris, List<string> allergener, int kvalité, int destination_bord)
+        Maträtt(string namn, int pris, int kvalité, int destination_bord)
         {
             Namn = namn;
             Pris = pris;
-            Allergener = allergener;
             Kvalité = kvalité;
             Destination_bord = destination_bord;
         }
